fefc1d8697293b248397c9c0601290467ae4a5a7
LeonMont <alejandroromero0048@gmail.com>
2020-12-20 16:19:35 -0500

    Proyecto 3 - Trapsoul

---
+++ js/script.js
@@ -0,0 +1,11 @@
+alert("¡Hola!");
+
+function mensajealerta() {
+    var mensaje = confirm("Sopresa! Dale aceptar si te gustó mi página");
+    if (mensaje) {
+        alert("¡Gracias!");
+    }
+    else {
+        alert("¡No tan rápido!");
+    }
+}
